42d925ce6dc876ac80acc12ef11eecaa22bca10c
Tech Madhurya <techmadhurya@gmail.com>
2026-03-06 16:13:31 +0530

    Base format

---
+++ src/data/sampleData.js
@@ -0,0 +1,14 @@
+export const items = [
+    {
+        id: 1,
+        itemName: "Bulb Stand",
+        category: "Lighting",
+        location: "Lakshmi Garden",
+        quantity: 4,
+        itemStatus: "Available",
+        tags: ["lighting", "sangeet"],
+        lastMoved: "2026-03-05",
+        BookedDates : []
+    },
+    {}
+]
